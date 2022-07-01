ccc14c1b7156b6426cdaf4526cbbab180e86d498
Davi-Mancebo <87788044+Davi-Mancebo@users.noreply.github.com>
2022-06-30 22:34:32 -0300

    Initial commit

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chao = new System.Windows.Forms.PictureBox();
+            this.flappyBird = new System.Windows.Forms.PictureBox();
+            this.pipeTop = new System.Windows.Forms.PictureBox();
+            this.pipeDown = new System.Windows.Forms.PictureBox();
+            this.lblPlacar = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblFim = new System.Windows.Forms.Label();
+            this.lblReiniciar = new System.Windows.Forms.Label();
+            this.ceu = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chao)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ceu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chao
+            // 
+            this.chao.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.chao.Image = ((System.Drawing.Image)(resources.GetObject("chao.Image")));
+            this.chao.Location = new System.Drawing.Point(0, 671);
+            this.chao.Name = "chao";
+            this.chao.Size = new System.Drawing.Size(694, 50);
+            this.chao.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.chao.TabIndex = 0;
+            this.chao.TabStop = false;
+            // 
+            // flappyBird
+            // 
+            this.flappyBird.Image = ((System.Drawing.Image)(resources.GetObject("flappyBird.Image")));
+            this.flappyBird.Location = new System.Drawing.Point(75, 320);
+            this.flappyBird.Name = "flappyBird";
+            this.flappyBird.Size = new System.Drawing.Size(80, 60);
+            this.flappyBird.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.flappyBird.TabIndex = 1;
+            this.flappyBird.TabStop = false;
+            // 
+            // pipeTop
+            // 
+            this.pipeTop.Image = ((System.Drawing.Image)(resources.GetObject("pipeTop.Image")));
+            this.pipeTop.Location = new System.Drawing.Point(481, 0);
+            this.pipeTop.Name = "pipeTop";
+            this.pipeTop.Size = new System.Drawing.Size(110, 276);
+            this.pipeTop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeTop.TabIndex = 2;
+            this.pipeTop.TabStop = false;
+            // 
+            // pipeDown
+            // 
+            this.pipeDown.Image = ((System.Drawing.Image)(resources.GetObject("pipeDown.Image")));
+            this.pipeDown.Location = new System.Drawing.Point(481, 450);
+            this.pipeDown.Name = "pipeDown";
+            this.pipeDown.Size = new System.Drawing.Size(110, 271);
+            this.pipeDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pipeDown.TabIndex = 3;
+            this.pipeDown.TabStop = false;
+            // 
+            // lblPlacar
+            // 
+            this.lblPlacar.AutoSize = true;
+            this.lblPlacar.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlacar.Location = new System.Drawing.Point(628, 22);
+            this.lblPlacar.Name = "lblPlacar";
+            this.lblPlacar.Size = new System.Drawing.Size(35, 39);
+            this.lblPlacar.TabIndex = 4;
+            this.lblPlacar.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblFim
+            // 
+            this.lblFim.AutoSize = true;
+            this.lblFim.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFim.Location = new System.Drawing.Point(248, 341);
+            this.lblFim.Name = "lblFim";
+            this.lblFim.Size = new System.Drawing.Size(0, 39);
+            this.lblFim.TabIndex = 6;
+            // 
+            // lblReiniciar
+            // 
+            this.lblReiniciar.AutoSize = true;
+            this.lblReiniciar.Font = new System.Drawing.Font("Microsoft YaHei UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReiniciar.Location = new System.Drawing.Point(248, 426);
+            this.lblReiniciar.Name = "lblReiniciar";
+            this.lblReiniciar.Size = new System.Drawing.Size(0, 39);
+            this.lblReiniciar.TabIndex = 8;
+            this.lblReiniciar.Click += new System.EventHandler(this.lblReiniciar_Click);
+            // 
+            // ceu
+            // 
+            this.ceu.BackColor = System.Drawing.Color.Transparent;
+            this.ceu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ceu.Location = new System.Drawing.Point(0, 0);
+            this.ceu.Name = "ceu";
+            this.ceu.Size = new System.Drawing.Size(694, 10);
+            this.ceu.TabIndex = 9;
+            this.ceu.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(694, 721);
+            this.Controls.Add(this.ceu);
+            this.Controls.Add(this.lblReiniciar);
+            this.Controls.Add(this.lblFim);
+            this.Controls.Add(this.lblPlacar);
+            this.Controls.Add(this.pipeTop);
+            this.Controls.Add(this.flappyBird);
+            this.Controls.Add(this.chao);
+            this.Controls.Add(this.pipeDown);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "FLAPPY BIRD";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.chao)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBird)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pipeDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ceu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox chao;
+        private System.Windows.Forms.PictureBox flappyBird;
+        private System.Windows.Forms.PictureBox pipeTop;
+        private System.Windows.Forms.PictureBox pipeDown;
+        private System.Windows.Forms.Label lblPlacar;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblFim;
+        private System.Windows.Forms.Label lblReiniciar;
+        private System.Windows.Forms.PictureBox ceu;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int gravidade = 5;
+        int pipeSpeed = 10;
+        int placar = 0;
+        Random pos = new Random();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            flappyBird.Top += gravidade;
+            pipeTop.Left -= pipeSpeed;
+            pipeDown.Left -= pipeSpeed;
+
+            
+                
+            if (pipeTop.Left < -100)
+            {
+                pipeTop.Left = 710;
+                
+            }
+            if (pipeDown.Left < -100)
+            {
+                pipeDown.Left = 710;
+                placar++;
+            }
+            lblPlacar.Text = placar.ToString();
+            if(flappyBird.Bounds.IntersectsWith(pipeTop.Bounds))
+            {
+                fimJogo();
+            }
+            if (flappyBird.Bounds.IntersectsWith(pipeDown.Bounds))
+            {
+                fimJogo();
+            }
+            if (flappyBird.Bounds.IntersectsWith(chao.Bounds))
+            {
+                fimJogo();
+            }
+            if (flappyBird.Bounds.IntersectsWith(ceu.Bounds))
+            {
+                fimJogo();
+            }
+        }
+
+        private void fimJogo()
+        {
+            timer1.Stop();
+            lblFim.Text = "Fim de jogo!";
+            lblReiniciar.Text = "reiniciar?";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) gravidade = -10;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) gravidade = 5;
+
+        }
+
+        private void lblReiniciar_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            placar = 0;
+            pipeSpeed = 5;
+            pipeTop.Left = 710;
+            pipeDown.Left = 710;
+            lblFim.Text = " ";
+            lblReiniciar.Text = ""; 
+        
+
+        }
+    }
+}
